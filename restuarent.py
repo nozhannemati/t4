59a38f706192ca5f217ed5e0b9a2fea27e9a2678
amir102800 <amirmohammadalizadeh1111@gmail.com>
2020-09-14 17:29:20 +0430

    2222

---
+++ restuarent.py
@@ -0,0 +1,27 @@
+import tkinter as tk               #
+import tkinter.ttk as ttk          #
+####################################
+root = tk.Tk()                     #
+####################################
+note = tk.notebook()               #
+note.grid(row=0, column=0)         #
+drink = tk.frame()                 #
+food = tk.frame()                  #
+recijept= tk.frame()               # 
+note.add(drink, text='drink')      #
+note.add(food, text='food')        #
+note.add(reciept, text=('reciept') #
+####################################
+root.mainloop()          
+          
+
+
+
+
+
+
+
+
+
+
+
