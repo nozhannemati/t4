cf960f2fccd53dc7ca4d269af99153be1fc75ee1
negarRahmanpoor2007@gmail.com <nojangamer83@gmai.com>
2020-09-16 17:25:46 +0430

    2222

--- restuarent.py
+++ restuarent.py
@@ -1,27 +1,43 @@
-import tkinter as tk               #
-import tkinter.ttk as ttk          #
-####################################
-root = tk.Tk()                     #
-####################################
-note = tk.notebook()               #
-note.grid(row=0, column=0)         #
-drink = tk.frame()                 #
-food = tk.frame()                  #
-recijept= tk.frame()               # 
-note.add(drink, text='drink')      #
-note.add(food, text='food')        #
-note.add(reciept, text=('reciept') #
-####################################
+import tkinter as tk               
+import tkinter.ttk as ttk          
+
+
+
+bgak ={"bg" :"#91ff35"}    
+bgak1 = {'bg' : "#f50057", 'font':'helvetica'}
+                                   
+                                   
+                                
+root = tk.Tk()                     
+
+
+
+note = ttk.Notebook()               
+note.grid(row=0, column=0)        
+
+
+
+food = tk.Frame()                 
+f1 = tk.Frame(food, cnf=bgak)
+f1.grid(row =0, column=0)
+
+reciept= tk.Frame()               
+
+
+drink = tk.Frame()                 
+
+
+
+note.add(drink, text='drink')     
+note.add(food, text='food')        
+note.add(reciept, text='reciept') 
+
+
+
+tk.Label(f1, text='ghorme sabzi',cnf=bgak).grid(row=0,column=0)
+tk.Label(f1, text='⭐⭐⭐⭐⭐(45)',cnf=bgak).grid(row=1, column=0)
+tk.Label(f1, text='1.2 $', cnf=bgak).grid(row=2, column=0)
+
+
+
 root.mainloop()          
-          
-
-
-
-
-
-
-
-
-
-
-
