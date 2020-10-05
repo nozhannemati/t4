7558a1c7e4ace1441c71ea25b82f2f4e97eff2a4
amir102800 <amirmohammadalizadeh1111@gmail.com>
2020-10-05 16:40:49 +0330

    222

--- restuarent.py
+++ restuarent.py
@@ -1,94 +1,65 @@
-import tkinter as tk
-from tkinter import PhotoImage
+import tkinter as tk 
 import tkinter.ttk as ttk
-from config import *
+from tkinter import PhotoImage
 
-label_cnf = {
-    'bg': '#ffc107',
-    'font': ('fixedsys', 20)
+# ################food information ##################### #
+
+i = {
+    "1" : {"name" :"Akbar Chicken",
+          "rating" : 5 , 
+          "review" : 47 ,
+          "price" : 2.58,
+          "img" : "restaurant/img/fd9e168e23ededfd719b7787fb3501ba.gif"} , 
+    "2" : {"name" :"Mahi Polo",
+          "rating" : 5 , 
+          "review" : 47 ,
+          "price" : 2.58,
+          "img" : "restaurant/img/fd9e168e23ededfd719b7787fb3501ba.gif" } 
 }
 
-# $$$$$$$$$$$$$$$$$$$ Food Information $$$$$$$$$$$$$$$$$$$ #
-i = {
-    '1': {'name': 'BaqaliQatoq',
-          'rating': 5,
-          'review': 47,
-          'price': 1.5,
-          'des':'This is Iranian Food, which most used in north of IRAN Elit reprehen derit exce pteur dolor labore ipsum veniam exercitation deserunt.',
-          'img': 'images/baqali.gif'},
-i = {
-    '1': {'name': 'sabziqorme',
-          'rating': 5,
-          'review': 12,
-          'price': 0.47,
-          'des':'This is Iranian Food, which most used in north of IRAN Elit reprehen derit exce pteur dolor labore ipsum veniam exercitation deserunt.',
-          'img': 'images/baqali.gif'},
-
-i = {
-    '1': {'name': 'gheyme',
-          'rating': 5,
-          'review': 42,
-          'price': 0.47,
-          'des':'This is Iranian Food, which most used in north of IRAN Elit reprehen derit exce pteur dolor labore ipsum veniam exercitation deserunt.',
-          'img': 'images/baqali.gif'},
+# ######################### Master ##################### #
+bgak = {"bg" : "#00897B"}
+bgak1 = {"bg" : "#FF5722" ,"font":'Helvetica'}
+bgak2 = {"bg" : "#CDDC39" ,"font":'Helvetica'}
+bgak3 = {"bg" : "#FF9800" ,"font":'Helvetica'}
 
 
-}
+root =tk.Tk()
 
-# -------------------------------------------------------- #
+root.title("ᴀᴋʙᴀʀ ᴄʜɪᴄᴋᴇɴ")
 
-root = tk.Tk()
-root.title('Restauran Menu System')
+
 note = ttk.Notebook()
-note.grid(row=0 , column=0)
-# ################ NoteBook Tabs ############### #
-food = tk.Frame()
-drink = tk.Frame()
-reciept = tk.Frame()
-# ---------------------------------------------- #
-note.add(food, text='Foods')
-note.add(drink, text='Foods')
-note.add(reciept, text='Foods')
-# ################## Food Tab ################## #
-f1 = tk.Frame(food,bg='#ffc107',
-)
-f1.grid(row=0, column=0)
-
-name = i['1']['name']
-tk.Label(f1,
-    text=name,
-    cnf=label_cnf,
-    anchor='w').grid(row=0, column=0, sticky=tk.W)
-
-rating = i['1']['rating'] * '★' + '(' + str(i['1']['review']) +')'
-tk.Label(f1,
-    text=rating,
-    font='fixedsys',
-    anchor='nw').grid(row=1, column=0, sticky="nw", padx=10, pady=5)
-
-des = i['1']['des']
-tk.Message(f1,
-    text=des,
-    font='fixedsys',
-    width=200).grid(row=2, column=0, columnspan=2, pady=5)
-
-price = str(i['1']['price']) + '$' 
-tk.Label(f1,
-    text=price,
-    font='fixedsys').grid(row=0, column=1)
-
-img = PhotoImage(file=i['1']['img'])
-tk.Label(f1,
-    image=img,
-    borderwidth=4,
-    relief="sunken",
-    highlightcolor="red").grid(row=0, column=2, rowspan=4, padx=5, stick=tk.S, pady=5)
+note.grid(row=0, column=0)
 
 
+food = tk.Frame() 
+
+drinks = tk.Frame()
+
+rescipt = tk.Frame()
+
+note.add(food , text = "FOOD")
+note.add(drinks , text = "DRINKS")
+note.add(rescipt , text = "RESCIPT")
+
+for b in range(len(i)):
+    food1 = tk.Frame(food , cnf=bgak )
+    food1.grid(row=b , column = 0 , sticky = tk.E+tk.W)
+
+    name = i["1"]["name"]
+    tk.Label(food1, text = name, cnf=bgak1 ).grid(row = 0 , column = 0 , pady = 5 , padx = 5 )
+
+    rating = i["1"]["rating"] * "★" + "(" + str(i["1"]["review"]) + ")"
+    tk.Label(food1, text = rating, cnf=bgak2 ).grid(row = 1 , column = 0 , pady = 5)
+
+    price = str(i["1"]["price"]) + "$"
+    tk.Label(food1, text = price , cnf=bgak3 ).grid(row = 2 , column = 0 , pady = 5 )
 
 
-
-
+    img = PhotoImage(file =i["1"]["img"]).subsample(2)
+    tk.Label(food1, image = img).grid(row = 0 , column = 1 , rowspan = 3 )
 
 
 root.mainloop()
+
